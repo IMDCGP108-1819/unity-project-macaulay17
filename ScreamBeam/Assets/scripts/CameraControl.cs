490be7e386e020f9143f417ee7f97ee3bb09e18e
macaulay17 <45171072+macaulay17@users.noreply.github.com>
2019-01-09 12:38:18 +0000

    added jump+platform

---
+++ ScreamBeam/Assets/scripts/CameraControl.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+    public Transform Player;
+
+	void LateUpdate () {
+        float playerX = Player.position.x;
+        transform.position = new Vector3(playerX, transform.position.y, transform.position.z);
+	}
+}
--- ScreamBeam/Assets/scripts/PlayerController.cs
+++ ScreamBeam/Assets/scripts/PlayerController.cs
@@ -6,10 +6,16 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
 	public float Horizontalmovement;
+    public float JumpForce;
 	public Rigidbody2D rb; 
 
 	void FixedUpdate () {
 		float horizontal = Input.GetAxis("Horizontal") * Horizontalmovement;
 		rb.velocity = new Vector2(horizontal, rb.velocity.y);
+
+        if ( Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(Vector2.up * JumpForce, ForceMode2D.Impulse);
+        }
 	}
 }
