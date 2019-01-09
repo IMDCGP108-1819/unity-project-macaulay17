3d94c721c8b5474b0c6147a51b1751b98ef2245b
macaulay17 <45171072+macaulay17@users.noreply.github.com>
2019-01-09 14:34:11 +0000

    player instructions added

--- ScreamBeam/Assets/scripts/Startingthegame.cs
+++ ScreamBeam/Assets/scripts/Startingthegame.cs
@@ -1,16 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class Startingthegame : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-}
+    private void OncollisiomEnter2d(Collision2D collision)
+    {
+        { 
+        if (collision.gameObject.CompareTag("player"))
+        {
+            SceneManager.LoadScene("main game")
+
+      ;
+        }
+
+
