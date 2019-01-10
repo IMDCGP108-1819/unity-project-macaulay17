f9e3005beb0819a08ddb4b297f69d31eb2e44233
macaulay17 <45171072+macaulay17@users.noreply.github.com>
2019-01-10 11:33:02 +0000

    fixed menu

---
+++ ScreamBeam/Assets/scripts/MenuControls.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuControls : MonoBehaviour {
+
+    public void OnInstructionClick()
+    {
+        SceneManager.LoadScene("instructions");
+    }
+
+    public void OnStartClick()
+    {
+        SceneManager.LoadScene("main game");
+    }
+
+    public void OnQuitClick()
+    {
+        Application.Quit();
+    }
+}
--- ScreamBeam/Assets/scripts/player_beam.cs
+++ ScreamBeam/Assets/scripts/player_beam.cs
@@ -2,15 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class player_beam : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+public class player_beam : MonoBehaviour
+{
 }
